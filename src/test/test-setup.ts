81dca4a5005acce1df14fa8f38ecccacc02a2300
Tilo <Tilo@Isabelles-MacBook-Pro.local>
2019-06-29 11:18:10 +0200

    Add more description to test-setup.ts.

--- src/test/test-setup.ts
+++ src/test/test-setup.ts
@@ -4,6 +4,8 @@ import { LocalServer } from "./local-server";
 /**
  *  Here you can define global test settings, like a global afterEach.
  * 
+ *  But be patient: you are in a single test scope. Each test file in jest is a own process and scope
+ *  That means you can't share variables, classes, constants or something else between two test f-iles.
  *  This file is defined in setupFilesAfterEnv in the jest.config.js file.
  */
 
