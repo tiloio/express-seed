cc5b60e2dfaf33ff24453ca3ef6f5d5bf37228a8
Tilo <Tilo@Isabelles-MacBook-Pro.local>
2019-06-29 13:31:28 +0200

    Add error response at async middleware if error occures.

--- src/api/async-middleware.ts
+++ src/api/async-middleware.ts
@@ -4,6 +4,8 @@ export const asyncMiddleware = (fn: Function) =>
     (req: express.Request, res: express.Response, next: express.NextFunction) => {
         Promise.resolve(fn(req, res, next)).catch((error) => {
             console.error(`Error occured at '${req.originalUrl}':`, error);
+            res.statusCode = 500;
+            res.send();
             next();
         });
     };
